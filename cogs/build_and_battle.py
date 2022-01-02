6696eb77f385716576a9929783cabd3bea499728
skyewong <skyewong.personal@gmail.com>
2022-01-02 21:44:59 +0800

    test

---
+++ cogs/build_and_battle.py
@@ -0,0 +1,243 @@
+import nextcord
+import json
+import random
+import asyncio
+import main
+import math 
+from main import bot
+from datetime import datetime
+from nextcord.ext import commands, tasks
+
+weathers = ['sunny', 'rainy', 'stormy', 'windy', 'snowy']
+crop_emojis = ['<:crop_1:919601339464560650>', '<:crop_2:919601339338735616>', '<:crop_3:919601339082879027>', '<:crop_4:919601339456180264>', '<:crop_5:919601339447799848>']
+i = 10
+crop_progress = []
+while i <= 16:
+    crop_progress.append(i / 1000)
+    i += 1
+
+class build_and_battle(commands.Cog, name='Build & Battle'):
+    """My game! ~~i can\'t think of this it will be done later~~"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    
+    def get_user_profile_list(self):
+        with open('./user_profile.json', 'r') as f:
+            user_profile_list = json.load(f)
+        return user_profile_list
+
+    def get_user_list(self):
+        with open('./user_profile.json', 'r') as f:
+            user_profile_list = json.load(f)
+        i = 0
+        users = []
+        while i < len(user_profile_list):
+            users.append(user_profile_list[i]['id'])
+            i += 1
+        return users
+    
+    def if_user_present(self, user):
+        with open('./user_profile.json', 'r') as f:
+            user_profile_list = json.load(f)
+        i = 0
+        users = []
+        while i < len(user_profile_list):
+            users.append(user_profile_list[i]['id'])
+            i += 1
+        if user.id in users:
+            return True
+
+    def get_user_profile(self, user):
+        with open('./user_profile.json', 'r') as f:
+            user_profile_list = json.load(f)
+        i = 0
+        users = []
+        while i < len(user_profile_list):
+            users.append(user_profile_list[i]['id'])
+            i += 1
+        user_profile = user_profile_list[users.index(user.id)]
+        user_profile['crops'] = user_profile['crops'].split(', ')
+        for i in user_profile['crops']:
+            user_profile['crops'][user_profile['crops'].index(i)] = float(i)
+        return user_profile
+
+    def update_user_profile(self, user, new_profile):
+        with open('./user_profile.json', 'r') as f:
+            user_profile_list = json.load(f)
+        i = 0
+        users = []
+        while i < len(user_profile_list):
+            users.append(user_profile_list[i]['id'])
+            i += 1
+        for i in new_profile['crops']:
+            new_profile['crops'][new_profile['crops'].index(i)] = str(i)
+        new_profile['crops'] = ', '.join(new_profile['crops'])
+        if user.id in users:
+            user_profile_list[users.index(user.id)] = new_profile
+        else:
+            user_profile_list.append(new_profile)
+        with open('./user_profile.json', 'w+') as f:
+            json.dump(user_profile_list, f, indent=4)
+        return new_profile
+
+    @commands.command(name='create')
+    async def create(self, ctx):
+        """Create your own profile to start playing the Build & Battle game!"""
+        if self.if_user_present(ctx.author):
+            await ctx.send('You already have a profile!')
+        else:
+            create_profile = {
+                'id': ctx.author.id,
+                'gold': 0,
+                'xp': 0,
+                'farm_width': 2,
+                'crops': [],
+                'farm_last_used': int(datetime.now().timestamp())
+            }
+            for i in range(pow(create_profile['farm_width'], 2)):
+                create_profile['crops'].append(1)
+            self.update_user_profile(ctx.author, create_profile)
+            await ctx.send('Profile sucessfully created! Check your profile with `+profile`!')
+
+    @commands.command(name='profile')
+    async def profile(self, ctx, user: nextcord.Member=None):
+        """Check the profile of your own or others.
+        If you left the `<user>` parameter blank, the bot shows your own profile.
+        Otherwise, it shows other users' profiles."""
+        if user == None:
+            user = ctx.author
+        if self.if_user_present(user):
+            user_profile = self.get_user_profile(user)
+            profile_ui = nextcord.Embed()
+            profile_ui.colour = random.choice(main.embed_colours)
+            profile_ui.set_author(name=f'{user.name}\'s Profile:', icon_url=user.avatar)
+            profile_ui.add_field(name='Gold', value=f'${user_profile["gold"]}', inline=False)
+            profile_ui.add_field(name='XP', value=f'{user_profile["xp"]}/{main.roundup(user_profile["xp"], 100) if user_profile["xp"] != 0 else 100}', inline=False)
+            farm_width = main.rounddown(user_profile['xp'], 100) / 100 + 2
+            if farm_width >= 12:
+                farm_width = 12
+            profile_ui.add_field(name='Farm Size', value=int(pow(farm_width, 2)), inline=False)
+            profile_message = await ctx.send(embed=profile_ui)
+        else:
+            await ctx.send('The user do not have a profile! Create one with `+create`')
+
+    def update_farm_ui(self, user):
+        user_profile = self.get_user_profile(user)
+        sent_time = int(datetime.now().timestamp())
+        with open('weathers.json', 'r') as f:
+            weather_list = json.load(f)
+        past_weather = weather_list[-(math.floor((sent_time - user_profile['farm_last_used']) / 60 / 60)):]
+        growth = 1
+        for i in past_weather:
+            if i == 'sunny':
+                growth *= 1.2
+            elif i == 'stormy':
+                growth *= .6
+            elif i == 'rainy':
+                growth *= 1.1
+            elif i == 'windy':
+                growth *= .8
+            elif i == 'snowy':
+                growth *= .8
+        for i in range(len(user_profile['crops'])):
+            user_profile['crops'][i] += (sent_time - user_profile['farm_last_used']) / 60 * random.choice(crop_progress) * growth
+            if user_profile['crops'][i] >= 4.5:
+                user_profile['crops'][i] = 5.0
+        user_profile['farm_last_used'] = sent_time
+        crop = ''
+        farm_ui = nextcord.Embed()
+        farm_ui.set_author(name=f'{user.name}\'s Farm:', icon_url=user.avatar)
+        farm_ui.description = ''
+        index = 0
+        farm_width = main.rounddown(user_profile['xp'], 100) / 100 + 2
+        if farm_width > 12:
+            farm_width = 12
+        farm_width = int(farm_width)
+        crops_to_modify = abs(pow(farm_width, 2) - len(user_profile['crops']))
+        if pow(farm_width, 2) > pow(user_profile['farm_width'], 2):
+            farm_ui.add_field(name='Your farm expanded!', value=f'Now your farm has {pow(farm_width, 2)} crops.', inline=False)
+            for i in range(crops_to_modify):
+                user_profile['crops'].append(1)
+        if pow(farm_width, 2) < pow(user_profile['farm_width'], 2):
+            farm_ui.add_field(name='Your farm shrunk!', value=f'Sorry for the inconvinence. We think that\'s a bug. Now your farm has {pow(farm_width, 2)} crops.', inline=False)
+            for i in range(crops_to_modify):
+                user_profile['crops'].pop()
+        for y in range(farm_width):
+            for x in range(farm_width):
+                crop = user_profile['crops'][index]
+                crop = round(crop)
+                farm_ui.description += crop_emojis[crop - 1]
+                index += 1
+            farm_ui.description += '\n'
+        user_profile['farm_width'] = farm_width
+        with open('weathers.json', 'r') as f:
+            weather_list = json.load(f)
+        farm_ui.colour = random.choice(main.embed_colours)
+        farm_ui.set_footer(text=f'weather: {weather_list.pop()}')
+        user_profile = self.update_user_profile(user, user_profile)
+        user_profile = self.get_user_profile(user)
+        return farm_ui, user_profile
+
+    @commands.command(name='farm', help='Farming!')
+    async def farm(self, ctx):
+        if self.if_user_present(ctx.author):
+            farm_ui, user_profile = self.update_farm_ui(ctx.author)
+            farm_embed = await ctx.send(embed=farm_ui)
+            if ctx.author == ctx.author:
+                emojis = ['🌾', '⏹️']
+            else:
+                emojis = ['⏹️']
+            for emoji in emojis:
+                await farm_embed.add_reaction(emoji)
+            reacted_emoji = ''
+            while reacted_emoji != '⏹️':
+                def check(reaction, user):
+                    for emoji in emojis:
+                        if str(reaction.emoji) == emoji:
+                            correct_emoji = True
+                    return user == ctx.author and correct_emoji
+                try:
+                    reaction, user = await bot.wait_for('reaction_add', timeout=15.0, check=check)
+                except asyncio.TimeoutError:
+                    break
+                else:
+                    reacted_emoji = str(reaction.emoji)
+                    await farm_embed.remove_reaction(reaction.emoji, ctx.author)
+                    farm_ui, user_profile = self.update_farm_ui(ctx.author)
+                    fully_grown_crops = 0
+                    if reacted_emoji == '🌾':
+                        for i in user_profile['crops']:
+                            if i == 5:
+                                fully_grown_crops += 1
+                                user_profile['crops'][user_profile['crops'].index(i)] = 1
+                        earned_gold = 10 * fully_grown_crops
+                        earned_xp = random.choice(range(1, 9)) * fully_grown_crops
+                        user_profile['gold'] += earned_gold
+                        user_profile['xp'] += earned_xp
+                        self.update_user_profile(ctx.author, user_profile)
+                        farm_ui, user_profile = self.update_farm_ui(ctx.author)
+                        farm_ui.add_field(name='Crop collected and sold!', value=f'You sold {fully_grown_crops} fully grown crop(s) so you earned ${earned_gold} and {earned_xp} XP!', inline=False)
+                        await farm_embed.edit(embed=farm_ui)
+            for emoji in emojis:
+                await farm_embed.remove_reaction(emoji, bot.user)
+        else:
+            await ctx.send('You do not have a profile! Create one with `+create`')
+    
+    @tasks.loop(minutes=1.0)
+    async def new_weather():
+        with open('weathers.json', 'r') as f:
+            weather_list = json.load(f)
+        if len(weather_list) > 48:
+            weather_list = []
+        if datetime.now().minute == 0 or weather_list == []:
+            current_weather = random.choice(weathers)
+            weather_list.append(current_weather)
+            with open('weathers.json', 'w+') as f:
+                json.dump(weather_list, f, indent=4)
+
+    new_weather.start()
+    
+def setup(bot: commands.Bot):
+    bot.add_cog(build_and_battle(bot))
---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import json 
+import datetime
+import time
+import random
+import asyncio
+import nextcord
+import logging
+from nextcord.ext import commands
+
+TOKEN = os.environ['DISCORD_TOKEN']
+
+logger = logging.getLogger('nextcord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='nextcord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+bot = commands.Bot(command_prefix='+', case_insensitive=True, activity=nextcord.Game(name='+help'), owner_id=806334528230129695)
+embed_colours = [0x0071ad, 0x0064a4, 0x007dbd, 0x0096d6, 0x19afef, 0x32c8ff]
+
+def roundup(number, round_to):
+    return number if number % round_to == 0 else number + round_to - number % round_to
+
+def rounddown(number, round_to):
+    return number if number % round_to == 0 else number - number % round_to
+                
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    print('Connected servers/guilds:')
+    for guild in bot.guilds:
+        print(f'  -{guild.name}(id={guild.id})')
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    if message.author == bot.user:
+        return
+    if isinstance(message.channel, nextcord.channel.DMChannel):
+        if not message.content.lower().startswith('+'):
+            author = message.author.name+message.author.discriminator
+            content = message.content
+            sent_time = message.created_at.strftime('%A, %B %d %Y @ %H:%M:%S %p')+' UTC+0'
+            message_data = [author, content, sent_time]
+            with open('dm.json', 'r') as f:
+                dm_messages = json.load(f)
+            with open('dm_backup.json', 'r') as f:
+                dm_backup_messages = json.load(f)
+            dm_messages.append(message_data)
+            dm_backup_messages.append(message_data)
+            with open('dm.json', 'w+') as f:
+                json.dump(dm_messages, f, indent=4)
+            with open('dm_backup.json', 'w+') as f:
+                json.dump(dm_backup_messages, f, indent=4)
+    bot_id = bot.user.id
+    if message.content.find(f'<@!{str(bot_id)}>') != -1:
+        response = 'Hello! I\'m SkyeBot, created by Skye Wong!'
+        await message.channel.send(response)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.load_extension('help_cogs.cog')
+
+bot.run(TOKEN)
