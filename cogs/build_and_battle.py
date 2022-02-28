d0617c418e9c2de434020a7e37b0d485df7c7c6a
SkyeWong <skyewong.personal@gmail.com>
2022-02-28 14:11:15 +0800

    F

--- cogs/build_and_battle.py
+++ cogs/build_and_battle.py
@@ -79,8 +79,8 @@ class build_and_battle(commands.Cog, name="Build & Battle"):
     
     @commands.command(name="connectdb")
     async def connectdb(self, ctx):
-        await ctx.send(db)
         print(db)
+        await ctx.send(db)
         if db == None:
             db = connect(
                 host='bsuvufmpxye5uuutqete-mysql.services.clever-cloud.com',
