d4d9466cebef2efa101416407ca01028b333633c
SkyeWong <skyewong.personal@gmail.com>
2022-03-02 18:52:57 +0800

    bugs again

--- db.py
+++ db.py
@@ -4,16 +4,17 @@ from mysql.connector import connect, Error
 conn = None
 
 def start():
-    db = connect(
+    conn = connect(
             host='bsuvufmpxye5uuutqete-mysql.services.clever-cloud.com',
             user='umjpzdqlwm5z2ht6',
             password= os.environ['MYSQL_PW'],
             database='bsuvufmpxye5uuutqete'
     )
-    print(db)
-    return(db)
+    print(conn)
+    return(conn)
 
 def execute(self, sql):
+    print("i'm running!")
     try:
       cursor = conn.cursor()
       cursor.execute(sql)
