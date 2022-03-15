284a5f21982599893a771ec50ef76ffe1a96afa1
skyewong <skyewong.personal@gmail.com>
2022-03-15 22:26:02 +0800

    after editing?

--- views/currency_views.py
+++ views/currency_views.py
@@ -82,7 +82,7 @@ class MultiplePages(View):
         )
         self.page_to_return = None
         async def go_back(go_back_interaction):
-            if self.page_to_return:
+            if self.page_to_return and [i for i in self.children if i.custom_id=="go_back"][0]:
                 page_ui = self.page_to_return
                 self.remove_item([i for i in self.children if i.custom_id=="go_back"][0])
                 self.page_to_return = None
@@ -139,12 +139,13 @@ class MultiplePages(View):
             await interaction.followup.send("This is not for you.", ephemeral=True)
             return False
         else:
-            # to_page_b_btn = [i for i in self.children if i.custom_id=="to_page_b"][0]
-            # if self.message.embeds[0] == self.pages.page_ui_b():
-            #     self.remove_item(to_page_b_btn)
-            #     self.add_item(self.go_back_btn)
-            # else:
-            #     self.add_item(to_page_b_btn)
-            # if self.page_to_return:
-            #     self.add_item(self.go_back_btn)
+            to_page_b_btn = [i for i in self.children if i.custom_id=="to_page_b"][0]
+            if self.message.embeds[0] == self.pages.page_ui_b():
+                self.remove_item(to_page_b_btn)
+                self.add_item(self.go_back_btn)
+            else:
+                self.add_item(to_page_b_btn)
+            if self.page_to_return:
+                self.add_item(self.go_back_btn)
+            await self.message.edit(view=self)
             return True
