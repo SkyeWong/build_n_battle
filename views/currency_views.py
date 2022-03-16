35ba9937881a75ceac8ab3089d26faa17998bee9
skyewong <93854496+SkyeWong@users.noreply.github.com>
2022-03-16 10:30:52 +0800

    Update currency_views.py

--- views/currency_views.py
+++ views/currency_views.py
@@ -74,8 +74,8 @@ class MultiplePages(View):
     def __init__(self, ctx, pages):
         super().__init__(timeout=30)
         self.ctx = ctx
-	self.pages = pages
-	self.page_to_return = None
+		self.pages = pages
+		self.page_to_return = None
         self.go_back_btn = Button(
 			label = "Go Back",
 			style = nextcord.ButtonStyle.grey,
