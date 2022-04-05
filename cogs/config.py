e20f7ff1363fd49cfd529fbd4eb730536c80aede
skyewong <skyewong.personal@gmail.com>
2022-04-05 19:24:54 +0800

    show the prefix is there is none

--- cogs/config.py
+++ cogs/config.py
@@ -22,58 +22,71 @@ class Config(commands.Cog, name="Config"):
     
     @commands.command(name="prefix")
     @commands.has_permissions(administrator=True)
-    async def prefix(self, ctx, prefix):
-        if len(prefix) > 15:
-            await ctx.send("A prefix can only be 15 characters or shorter.")
+    async def prefix(self, ctx, prefix: str=None):
+        if prefix == None:
+            sql = f"""
+            SELECT prefix
+            FROM server_prefixes
+            WHERE server_id = {ctx.guild.id}
+            """
+            cursor = db.execute_query(sql)
+            prefix = cursor.fetchall()[0][1]
+            await ctx.send(f"The current prefix is `{prefix}`. eg `{prefix}help`")
         else:
-            prefix_ui = Embed()
-            prefix_ui.colour = random.choice(main.embed_colours)
-            def get_embed_description(prefix_desc):
-                return f"Server: `{ctx.guild.name}`\nPrefix: `{prefix_desc}`"
-            def change_prefix(prefix):
-                sql = f"""
-                UPDATE 
-                    server_prefixes
-                SET
-                    prefix = "{prefix}"
-                WHERE
-                    server_id = {ctx.guild.id}
-                """
-                db.execute_query(sql)
-                db.conn.commit()
-            prefix_ui.description = get_embed_description(prefix)
-            if str(prefix[-1]).isalpha():
-                prefix_ui.add_field(name="Add a space?", value="I noticed that the last character is a letter. Do you want me to add a space after it?")
-                class ChangePrefix():
-                    def __init__(self, ctx, prefix):
-                        self.ctx = ctx
-                        self.prefix = prefix
+            if ctx.author.guild_permissions.administrator:
+                if len(prefix) > 15:
+                    await ctx.send("A prefix can only be 15 characters or shorter.")
+                else:
+                    prefix_ui = Embed()
+                    prefix_ui.colour = random.choice(main.embed_colours)
+                    def get_embed_description(prefix_desc):
+                        return f"Server: `{ctx.guild.name}`\nPrefix: `{prefix_desc}`"
+                    def change_prefix(prefix):
+                        sql = f"""
+                        UPDATE 
+                            server_prefixes
+                        SET
+                            prefix = "{prefix}"
+                        WHERE
+                            server_id = {ctx.guild.id}
+                        """
+                        db.execute_query(sql)
+                        db.conn.commit()
+                    prefix_ui.description = get_embed_description(prefix)
+                    if str(prefix[-1]).isalpha():
+                        prefix_ui.add_field(name="Add a space?", value="I noticed that the last character is a letter. Do you want me to add a space after it?")
+                        class ChangePrefix():
+                            def __init__(self, ctx, prefix):
+                                self.ctx = ctx
+                                self.prefix = prefix
 
-                    def confirm_page(self):
-                        self.prefix += " "
-                        change_prefix(self.prefix)
-                        page = Embed()
-                        page.title = "Prefix set!"
-                        page.description = get_embed_description(self.prefix)
-                        page.add_field(name="Great! ",value="I added a space.")
-                        return page
+                            def confirm_page(self):
+                                self.prefix += " "
+                                change_prefix(self.prefix)
+                                page = Embed()
+                                page.title = "Prefix set!"
+                                page.description = get_embed_description(self.prefix)
+                                page.add_field(name="Great!",value="I added a space.")
+                                return page
+                            
+                            def cancel_page(self):
+                                change_prefix(self.prefix)
+                                page = Embed()
+                                page.title = "Prefix set!"
+                                page.description = get_embed_description(self.prefix)
+                                page.add_field(name="Fine.",value="The space is not added.")
+                                return page
                     
-                    def cancel_page(self):
-                        change_prefix(self.prefix)
-                        page = Embed()
-                        page.title = "Prefix set!"
-                        page.description = get_embed_description(self.prefix)
-                        page.add_field(name="Fine. ",value="The space is not added.")
-                        return page
-            
-                changeprefix = ChangePrefix(ctx, prefix)
-                view = AddSpaceInPrefix(ctx, changeprefix)
-                prefix_ui.title = "Setting the prefix:"
-                view.message = await ctx.send(embed=prefix_ui, view=view)
+                        changeprefix = ChangePrefix(ctx, prefix)
+                        view = AddSpaceInPrefix(ctx, changeprefix)
+                        prefix_ui.title = "Setting the prefix:"
+                        view.message = await ctx.send(embed=prefix_ui, view=view)
+                    else:
+                        change_prefix(prefix)
+                        prefix_ui.title = "Prefix set!"
+                        await ctx.send(embed=prefix_ui)
             else:
-                change_prefix(prefix)
-                prefix_ui.title = "Prefix set!"
-                await ctx.send(embed=prefix_ui)
+                await ctx.send("Sorry you are not an `administrator`. Contact with your owner to change the prefix.")
 
 def setup(bot: commands.Bot):
     bot.add_cog(Config(bot))
