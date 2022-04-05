385cee6816b8bd46fcb80bd886792c0fd8a98bc8
skyewong <skyewong.personal@gmail.com>
2022-04-05 18:56:24 +0800

    update

--- cogs/config.py
+++ cogs/config.py
@@ -1,5 +1,6 @@
 from curses.ascii import isalpha
 import os
+from discord import ButtonStyle
 import nextcord
 import random
 import main
@@ -11,7 +12,7 @@ from nextcord.ui import Button, View
 import database as db
 from typing import Optional
 from functions.users import Users
-
+from views.config_views import AddSpaceInPrefix
 class Config(commands.Cog, name="Config"):
     COG_EMOJI = "⚙️"
 
@@ -25,19 +26,37 @@ class Config(commands.Cog, name="Config"):
         if len(prefix) > 15:
             await ctx.send("A prefix can only be 15 characters or shorter.")
         else:
-            if prefix[-1].isaplha():
-                prefix += " "
-            sql = f"""
-                UPDATE 
-                    server_prefixes
-                SET
-                    prefix = "{prefix}"
-                WHERE
-                    server_id = {ctx.guild.id}
-                """
-            db.execute_query(sql)
-            db.conn.commit()
-            await ctx.send(f"The prefix for `{ctx.guild.name}` has been changed to \"`{prefix}`\"")
+            prefix_ui = Embed()
+            prefix_ui.title = f"Setting the prefix:"
+            def get_embed_description():
+                return f"Server: `{ctx.guild.name}`\nPrefix: `{prefix}`"
+            prefix_ui.description = get_embed_description()
+            if str(prefix[-1]).isalpha():
+                prefix_ui.add_field(name="Add a space?", value="I noticed that the last character is a letter. Do you want me to add a space after it?")
+                class ChangePrefix():
+                    def __init__(self, ctx, prefix):
+                        self.ctx = ctx
+                        self.prefix = prefix
+
+                    def confirm_page(self):
+                        self.prefix += " "
+                        page = Embed()
+                        page.title = prefix_ui.title
+                        page.description = get_embed_description()
+                        page.add_field(name="Great! I added a space.")
+                        return page
+                    
+                    def cancel_page(self):
+                        page = Embed()
+                        page.title = prefix_ui.title
+                        page.description = get_embed_description()
+                        page.add_field(name="Fine. The space is not added.")
+                        return page
+                        
+                changeprefix = ChangePrefix(ctx, prefix)
+                view = AddSpaceInPrefix(ctx, ChangePrefix)
+                view.message = await ctx.send(embed=prefix_ui, view=view)
+            await ctx.send(embed=prefix_ui)
 
 def setup(bot: commands.Bot):
     bot.add_cog(Config(bot))
---
+++ views/config_views.py
@@ -0,0 +1,52 @@
+import os
+import nextcord
+import random
+import main
+import math 
+from main import bot
+from datetime import datetime
+from nextcord.ext import commands
+from nextcord import Embed, SelectOption 
+from nextcord.ui import Button, View
+import database as db
+from typing import Optional
+from functions.users import Users
+
+class AddSpaceInPrefix(View):
+
+    def __init__(self, ctx, pages):
+        super().__init__(timeout=60)
+        self.ctx = ctx
+        self.pages = pages
+
+    @nextcord.ui.button(
+        label = "Cancel", 
+        style = nextcord.ButtonStyle.red, 
+        emoji = "❌"
+    )
+    async def cancel(self, button, interaction):
+        for i in self.children:
+            i.disabled = True
+        await interaction.response.edit_message(embed=self.pages.cancel_page(), view=self)
+
+    @nextcord.ui.button(
+        label = "Confirm", 
+        style = nextcord.ButtonStyle.green, 
+        emoji = "✔️"
+    )
+    async def confirm(self, button, interaction):
+        for i in self.children:
+            i.disabled = True
+        await interaction.response.edit_message(embed=self.pages.confirm_page(), view=self)
+    
+    async def on_timeout(self) -> None:
+        for i in self.children:
+            i.disabled = True
+        await self.message.edit(view=self)
+
+    async def interaction_check(self, interaction) -> bool:
+        if interaction.user != self.ctx.author:
+            await interaction.response.send_message(f"This is not for you, sorry.\nUse `{self.ctx.command}`", ephemeral=True)
+            return False
+        else:
+            return True
