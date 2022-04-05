e4e6e2eda5b281749e44dbc94c0531ccf9986553
skyewong <skyewong.personal@gmail.com>
2022-04-05 19:05:52 +0800

    pls

--- cogs/config.py
+++ cogs/config.py
@@ -27,10 +27,21 @@ class Config(commands.Cog, name="Config"):
             await ctx.send("A prefix can only be 15 characters or shorter.")
         else:
             prefix_ui = Embed()
-            prefix_ui.title = f"Setting the prefix:"
-            def get_embed_description():
-                return f"Server: `{ctx.guild.name}`\nPrefix: `{prefix}`"
-            prefix_ui.description = get_embed_description()
+            prefix_ui.colour = random.choice(main.embed_colours)
+            def get_embed_description(prefix_desc):
+                return f"Server: `{ctx.guild.name}`\nPrefix: `{prefix_desc}`"
+            def change_prefix(prefix):
+                sql = f"""
+                UPDATE 
+                    server_prefixes
+                SET
+                    prefix = "{prefix}"
+                WHERE
+                    server_id = {ctx.guild.id}
+                """
+                db.execute_query(sql)
+                db.conn.commit()
+            prefix_ui.description = get_embed_description(prefix)
             if str(prefix[-1]).isalpha():
                 prefix_ui.add_field(name="Add a space?", value="I noticed that the last character is a letter. Do you want me to add a space after it?")
                 class ChangePrefix():
@@ -40,23 +51,28 @@ class Config(commands.Cog, name="Config"):
 
                     def confirm_page(self):
                         self.prefix += " "
+                        change_prefix(self.prefix)
                         page = Embed()
-                        page.title = prefix_ui.title
-                        page.description = get_embed_description()
+                        page.title = "Prefix set!"
+                        page.description = get_embed_description(self.prefix)
                         page.add_field(name="Great! I added a space.")
                         return page
                     
                     def cancel_page(self):
+                        change_prefix(self.prefix)
                         page = Embed()
-                        page.title = prefix_ui.title
-                        page.description = get_embed_description()
+                        page.title = "Prefix set!"
+                        page.description = get_embed_description(self.prefix)
                         page.add_field(name="Fine. The space is not added.")
                         return page
-                        
+            
                 changeprefix = ChangePrefix(ctx, prefix)
-                view = AddSpaceInPrefix(ctx, ChangePrefix)
+                view = AddSpaceInPrefix(ctx, changeprefix)
+                prefix_ui.title = "Setting the prefix:"
                 view.message = await ctx.send(embed=prefix_ui, view=view)
-            await ctx.send(embed=prefix_ui)
+            else:
+                prefix_ui.title = "Prefix set!"
+                await ctx.send(embed=prefix_ui)
 
 def setup(bot: commands.Bot):
     bot.add_cog(Config(bot))
