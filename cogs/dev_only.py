448181242f346cfdb08d03ae71bf718bf497f370
skyewong <skyewong.personal@gmail.com>
2022-04-14 22:35:52 +0800

    will this work?

--- cogs/dev_only.py
+++ cogs/dev_only.py
@@ -24,7 +24,7 @@ class dev_only(commands.Cog, name="Dev Only"):
         #Check if user is owner and return it back
         return ctx.author.id in bot.owner_ids
 
-    @bot.slash_command(name="dm", description="Send a message! also this is my first slash command")
+    @nextcord.slash_command(name="dm", description="Send a message! also this is my first slash command", guild_ids=[main.DEVS_SERVER_ID])
     async def dm(self, interaction:Interaction, recipient: nextcord.Member, *, message: str):
         if recipient.bot == True:
             await interaction.response.send_message("I can't send a message to a BOT can i")
--- cogs/utility.py
+++ cogs/utility.py
@@ -1,5 +1,4 @@
 import os
-from discord import ButtonStyle
 import nextcord
 import json 
 from datetime import datetime
@@ -9,7 +8,7 @@ import asyncio
 import main
 from main import bot
 from nextcord.ext import commands, tasks
-from nextcord import Embed, Interaction
+from nextcord import Embed, Interaction, ButtonStyle
 from nextcord.ui import Button, View
 
 class Utility(commands.Cog, name="Utility"):
@@ -20,16 +19,16 @@ class Utility(commands.Cog, name="Utility"):
         self.bot = bot
         self._last_member = None 
 
-    @nextcord.slash_command(name="invite", description="Invite me!")
+    @nextcord.slash_command(name="invite", description="Invite me!", guild_ids=[main.DEVS_SERVER_ID])
     async def invite(self, interaction:Interaction):
-        embed = nextcord.Embed()
+        embed = Embed()
         embed.title = "Invite me to your server and have some fun!"
         embed.set_author(name=bot.user.name, icon_url= bot.user.avatar)
         embed.description = "[here](https://discord.com/api/oauth2/authorize?client_id=906505022441918485&permissions=8&scope=bot)"
         embed.colour = random.choice(main.embed_colours)
         await interaction.response.send_message(embed=embed)
 
-    @nextcord.slash_command(name="help", description="Get a list of commands or info of a specific command.")
+    @nextcord.slash_command(name="help", description="Get a list of commands or info of a specific command.", guild_ids=[main.DEVS_SERVER_ID])
     async def help(self, interaction:Interaction):
         await interaction.response.send_message("hi")
         msg = ""
