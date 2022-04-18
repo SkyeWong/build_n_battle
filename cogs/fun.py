640bb2257e2276cfb9e5d216642b5a2d11ea1e3b
skyewong <skyewong.personal@gmail.com>
2022-04-18 19:19:46 +0800

    most and least!

--- cogs/fun.py
+++ cogs/fun.py
@@ -51,13 +51,22 @@ class Fun(commands.Cog, name="Fun"):
                 str(random.choice(range(1, sides + 1)))
                 for _ in range(dice)
             ]
-        embed = nextcord.Embed()
+        embed = Embed()
         embed.title = f"I rolled {dice} dice with {sides} sides and the result is:"
         if dice > 5:
+            most = 0
+            least = 800
             descr = "As there are more than 5 dice, I counted the results for you! 😊 ```css"
             for side in range(1, sides + 1):
-                descr += f"\n* ({result.count(str(side))}) [{side}s]"
-            descr += "```"
+                count = result.count(str(side))
+                descr += f"\n* ({count}) [{side}s]"
+                if count > result.count(str(most)):
+                    most = side
+                if count < result.count(str(least)):
+                    least = side
+            descr += "```\n"
+            descr += f"`-` MOST: {most} with {result.count(str(most))} dice\n"
+            descr += f"`-` LEAST: {least} with {result.count(str(least))} dice\n"
         else:
             descr = ", ".join(result)
         embed.description = descr
@@ -66,7 +75,7 @@ class Fun(commands.Cog, name="Fun"):
 
     @nextcord.slash_command(name="coinflip", description="Flip a coin!", guild_ids=[main.DEVS_SERVER_ID])
     async def coinflip(self, interaction: Interaction):
-        embed = nextcord.Embed()
+        embed = Embed()
         embed.title = "Flippin' a coin..."
         embed.colour = random.choice(main.embed_colours)
         result = random.choice(range(2))
@@ -87,7 +96,7 @@ class Fun(commands.Cog, name="Fun"):
 
     @nextcord.slash_command(name="karson", description="Shows Karson in a big collage!", guild_ids=[main.DEVS_SERVER_ID])
     async def karson(self, interaction: Interaction):
-        embed = nextcord.Embed()
+        embed = Embed()
         embed.set_image(url="https://i.ibb.co/vzRD2LC/big-collage.jpg")
         karson_user = await bot.fetch_user(708141816020729867)
         embed.set_author(name="MEET KARSON:", icon_url= karson_user.avatar)
@@ -130,7 +139,7 @@ class Fun(commands.Cog, name="Fun"):
     async def dicegame(self, ctx):
         money = 500
         author_avatar = ctx.author.avatar
-        dice_ui = nextcord.Embed()
+        dice_ui = Embed()
         dice_ui.colour = random.choice(main.embed_colours)
         dice_ui.set_author(name=f"{ctx.author.name}'s Dicegame", icon_url= author_avatar)
         dice_ui_message = await ctx.send(embed=dice_ui)
