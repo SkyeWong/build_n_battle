dc012bf10643aa59013ba7c5d5c1e77705755742
skyewong <skyewong.personal@gmail.com>
2022-04-23 19:11:59 +0800

    new emoji?

--- cogs/dev_only.py
+++ cogs/dev_only.py
@@ -1,4 +1,5 @@
 import os
+from discord import SlashOption
 from nextcord import ButtonStyle
 import nextcord
 import json 
@@ -6,6 +7,8 @@ from datetime import datetime
 import time
 import random
 import asyncio
+
+from sqlalchemy import desc
 import main
 from main import bot
 from nextcord.ext import commands, tasks
@@ -23,58 +26,81 @@ class dev_only(commands.Cog, name="Dev Only"):
     async def cog_check(self, ctx):
         #Check if user is owner and return it back
         return ctx.author.id in bot.owner_ids
+    
+    async def cog_application_command_check(self, interaction):
+        return interaction.user.id in bot.owner_ids
 
     @nextcord.slash_command(name="dm", description="Send a message!", guild_ids=[main.DEVS_SERVER_ID])
     async def dm(self, interaction: Interaction, recipient: nextcord.Member, message: str):
-        if interaction.user.id not in self.bot.owner_ids:
-            await interaction.response.send_message("you are not a dev.")
+        if recipient.bot == True:
+            await interaction.response.send_message("i don't want to msg it, its ugly")
         else:
-            if recipient.bot == True:
-                await interaction.response.send_message("I can't send a message to a BOT can i")
-            else:
-                await recipient.send(f"{interaction.user} sent a message to you via me, {bot.user.name}:\n {message}")
-                embed = Embed()
-                embed.title = "DM succesfully sent!"
-                embed.set_author(name=bot.user.name, icon_url=bot.user.avatar)
-                embed.description = "Message details:"
-                embed.colour = random.choice(main.embed_colours)
-                embed.add_field(name="From:", value=interaction.user, inline=True)
-                embed.add_field(name="To:", value=recipient, inline=True)
-                embed.add_field(name="Message:", value=message, inline=True)
-                embed.add_field(name="Sent at:", value=f'<t:{int(datetime.now().timestamp())}>', inline=True)
-                embed.set_footer(text="Note: markdowns and mentions will be escaped while sending the message!")
-                await interaction.response.send_message(embed=embed)
+            await recipient.send(f"{interaction.user} sent a message to you via me, {bot.user.name}:\n {message}")
+            embed = Embed()
+            embed.title = "DM succesfully sent!"
+            embed.set_author(name=bot.user.name, icon_url=bot.user.avatar)
+            embed.description = "Message details:"
+            embed.colour = random.choice(main.embed_colours)
+            embed.add_field(name="From:", value=interaction.user, inline=True)
+            embed.add_field(name="To:", value=recipient, inline=True)
+            embed.add_field(name="Message:", value=message, inline=True)
+            embed.add_field(name="Sent at:", value=f'<t:{int(datetime.now().timestamp())}>', inline=True)
+            embed.set_footer(text="Note: markdowns and mentions will be escaped while sending the message!")
+            await interaction.response.send_message(embed=embed)
 
-    @commands.command(name="avatar", help="Shows avatar!")
-    async def avatar(self, ctx, user: nextcord.Member=None):
+    @nextcord.slash_command(name="avatar", description="Shows avatar of a user.", guild_ids=[main.DEVS_SERVER_ID])
+    async def avatar(
+        self, 
+        interaction: Interaction, 
+        user: nextcord.Member = SlashOption(
+            name = "user",
+            description = "The user whom you wants to have avatar shown. Defaults to... YOU!",
+            default = None
+        )
+    ):
         if user == None:
-            user = ctx.author
-        await ctx.send(f"<{user.display_avatar.url}>")
-        await ctx.send(user.display_avatar.url)
+            user = interaction.user
+        await interaction.response.send_message(f"<{user.display_avatar.url}>")
+        await interaction.followup.send(user.display_avatar.url)
 
-    @commands.command(name="emoji")
-    async def emoji(self, ctx, *, emojiname:str):
-        """Let the bot find you any emojis in any servers that the bot is in.
-        Search the emoji by typing the emoji name. Seperate emojis with ",".
-        Please note that the bot ONLY searches for server emojis. Default emojis will **NOT** be searched."""
-        emojis = emojiname.split(", ")
+    @nextcord.slash_command(name="emoji", description="Search for emojis in servers the bot is in.", guild_ids=[main.DEVS_SERVER_ID])
+    async def emoji(
+        self, 
+        interaction: Interaction,
+        emojiname: str = SlashOption(
+            name = "emoji",
+            description = "Emoji to search for, its id or name"
+        )):
         emojis_found = []
         guild_emojis = []
         for guild in bot.guilds:
             for emoji in guild.emojis:
                 guild_emojis.append(emoji)
-        for i in emojis:
-            emojis_found.append(nextcord.utils.get(guild_emojis, name=i))
-        response = "The emoji(s) you required should be: "
-        for i in range(len(emojis_found)):
-            emoji = emojis_found[i]
-            response += f"\n\n{i+1}: "
-            if emojis_found[i] == None:
-                response += f"No emoji is found for \"{emojis[i]}\"! Check the name and try again. Is it a default emoji? The bot only searches for server emojis."
-            else:
-                response += f"{emoji}\n> Name - :{emoji.name}:\n> Guild - {emoji.guild.name}\n> ID - `{emoji.id}`\n> Url - `{emoji.url}`"
-        response += "\n\nThe bot only checks for the first match of the emoji. Other emojis with identical names won't be found."
-        await ctx.send(response)
+        for emoji in guild_emojis:
+            if emojiname in emoji.name or emojiname == emoji.id:
+                emojis_found.append(emoji)
+        if emojis_found != []:
+            await interaction.response.send_message(f"There are `{len(emojis_found)}` results for `{emojiname}`:")
+            embed = Embed()
+            embed.set_author(name="Emoji Searcher:", icon_url=bot.user.display_avatar.url)
+            embed.colour = random.choice(main.embed_colours)
+            for i in range(len(emojis_found)):
+                emoji_embed = embed
+                emoji_embed.title = f"`{i+1}` - click for emoji"
+                emoji_embed.url = emoji.url
+                emoji_embed.description = f"{emoji}"
+                emoji = emojis_found[i]
+                field = ">>> "
+                field += f"➼ `Name` - :{emoji.name}:"
+                field += f"➼ `Guild` - `{emoji.guild.name}`"
+                field += f"➼ `ID`    - `{emoji.id}`"
+                field += f"➼ `Url`   - [{emoji.url}]({emoji.url})"
+                field += f"➼ `Usage` - `<:{emoji.name}:{emoji.id}>"
+                embed.add_field(name=f":{emoji.name}:", value=field)
+                await interaction.followup.send(embed=emoji_embed)
+            await interaction.followup.send(f"Jump to the start: {interaction.original_message.jump_url}")
+        else:
+            await interaction.response.send_message(f"No emoji is found for `{emojiname}`.", delete_after=5)
 
 def setup(bot: commands.Bot):
     bot.add_cog(dev_only(bot))
