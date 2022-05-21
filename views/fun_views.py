f66996011d2e23c033e531fde49ab7a2be464eb1
skyewong <skyewong.personal@gmail.com>
2022-05-21 20:43:17 +0800

    fine. stop it

--- views/fun_views.py
+++ views/fun_views.py
@@ -288,7 +288,7 @@ class HappyBirthdayView(View):
             if not channel:
                 channel = await interaction.user.create_dm()
             message = await channel.history(limit=20).flatten()
-            message = [i for i in message if i.author == interaction.client][0]
+            message = [i for i in message if i.author == bot.user][0]
             if message.author == bot.user and int(datetime.now().timestamp()) - int(message.created_at.timestamp()) < 5:
                 spamming = True
             if not self.stopped and not spamming:
